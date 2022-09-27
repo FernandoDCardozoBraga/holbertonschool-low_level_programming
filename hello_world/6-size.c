0cc210ddcadc57c56182451c9d0e9e69909f7690
FernandoDCardozoBraga <fer.car.br@gmail.com>
2022-09-27 15:01:22 -0700

    exercice 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * * main - Prints the many var types
+ * *
+ * * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+
+
+	printf("Size of a char: %lu byte(s)\n; (unisigned long)sizeof(a))";
+	printf("Size of an int: %lu byte(s)\n, (unisigned long)sizeof(b))";
+	printf("Size of a long int: %lu byte(s)\n, (unisigned long)sizeof(c))";
+	printf("Size of a long long int: %lu byte(s)\n, (unisigned long)sizeof(d))";
+	printf("Size of a float: %lu byte(s)\n, (unisigned long)sizeof(e));
+	return (0);
+
+}

46fe04f813cbe0780aa3f29583b686632341e482
FernandoDCardozoBraga <fer.car.br@gmail.com>
2022-09-29 09:53:37 -0700

    prints the alphabet in lowercase, and then in uppercase, followed by a new line

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Assign a random number to the variable n.
+ * Retour: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	putchar("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z\n");
+	for (i = 'A'; i <= 'Z'; i++)
+	putchar("%c ", i);
+	return 0;
+}

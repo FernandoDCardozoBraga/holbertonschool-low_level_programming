5f39a62b782c4316c45e2f1a5787a21ad432266b
FernandoDCardozoBraga <fer.car.br@gmail.com>
2022-10-11 14:54:35 -0700

    ex_4to7

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strncpy - function that copies a string
+ *@src: input
+ *@dest: input
+ *@n: input
+ *Return: A pointer to the resulting string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
+
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
+
+	srclen++;
+
+	if (n > srclen)
+		n = srclen;
+
+	src = start;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	return (temp);
+}

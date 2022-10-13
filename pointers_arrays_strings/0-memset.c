8ff910eb608d5ba9e3509c204e98730db93af4c7
FernandoDCardozoBraga <fer.car.br@gmail.com>
2022-10-13 09:45:13 -0700

    ex-0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_memset - function fills the first n bytes of
+ *the memory area pointed to by s with the constant byte b
+ *@s: input
+ *@n: output
+ *@b: input
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ferchu = s;
+
+		while (n--)
+		{
+			*s = b;
+			s++;
+		}
+	return (ferchu);
+}

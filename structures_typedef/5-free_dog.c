af54f99eed70e9cdeb4c3d02acf502d84594fdee
FernandoDCardozoBraga <fer.car.br@gmail.com>
2022-11-01 19:39:06 -0700

    ex_0_to_5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *free_dog-free struct
+ *@d: pointer to struct
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
